f15b3c9ae0dae42d60251e8644851fafabc7adf1
Denislavirgul <lavirgul@gmail.com>
2023-03-28 21:22:33 -0300

    Comienzo del proyecto

---
+++ miportfolio/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
